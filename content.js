7d27bd39d51ebf0c0d4fabb1627f48e6b7d04c7f
Pranay Mittal <pranaym12@gmail.com>
2018-05-04 12:34:31 -0700

    Clean-up comments.

--- content.js
+++ content.js
@@ -3,12 +3,15 @@ var isPostsContent = false;
 //use event listener to see if comments enabled
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.isCommented) {	
+		//this is the class that controls the # of likes shown on comments
 		$('.UFICommentLikeButton').css('display', "none");
 	}
 	if(request.isCommented == false) {
 		$('.UFICommentLikeButton').css('display', "inline");
 	}
 	if(request.isPosted){
+		//1st is class that controls the names of friends who've liked a post
+		//2nd is class that controls # of ppl besides your friends who've liked post
 		$('._1g5v').css('display', "none");
 		$('._4arz').css('display', "none");		
 	}
