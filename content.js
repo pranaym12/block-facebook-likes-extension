567e9f0bcdc1c7408c3506e734b64ea46eaa13f6
Pranay Mittal <pranaym12@gmail.com>
2018-05-03 17:09:30 -0700

    Remove the call to storage from content.js.

--- content.js
+++ content.js
@@ -1,13 +1,3 @@
-//check storage to see if comments are enabled
-chrome.storage.sync.get(['commentsClicked'], function(internal){
-	if(internal.commentsClicked){
-		console.log("Storage: comments ARE clicked");
-	}
-	else {
-		console.log("Storage: comments NOT clicked.");
-	}
-});
-
 //use event listener to see if comments enabled
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.isCommented) {
