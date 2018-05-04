52e68beee57efd222aeda3835c55b7f58d9a1308
Pranay Mittal <pranaym12@gmail.com>
2018-05-03 17:09:07 -0700

    Send message to popup from content when comments is toggled on or off.

--- content.js
+++ content.js
@@ -1,2 +1,19 @@
-console.log("This is a test.");
-//try to use document.getElementBYId to add a button?
+//check storage to see if comments are enabled
+chrome.storage.sync.get(['commentsClicked'], function(internal){
+	if(internal.commentsClicked){
+		console.log("Storage: comments ARE clicked");
+	}
+	else {
+		console.log("Storage: comments NOT clicked.");
+	}
+});
+
+//use event listener to see if comments enabled
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(request.isCommented) {
+		console.log("Event listener: comments ARE clicked");
+	}
+	else {
+		console.log("Event listener: comments NOT clicked");
+	}
+});
--- popup.js
+++ popup.js
@@ -1,41 +1,47 @@
 $(function(){
 	//Check for existing stored values, and set text in popup to that
-	chrome.storage.sync.get(['clickNumber', 'isClicked'], function(internal){
+	chrome.storage.sync.get(['clickNumber', 'commentsClicked'], function(internal){
 		//If clickNumber or truthHeader already exists, change the text to it
 		if(internal.clickNumber){ 
 			$('#clickCounter').text("Number of clicks: " + internal.clickNumber);
 		}
-		if(internal.isClicked){
-			$('#truthHeader').text(internal.isClicked.toString());
+		if(internal.commentsClicked){
+			$('#truthHeader').text(internal.commentsClicked.toString());
 		}
 	});
 
-	//If the button is clicked, increment the clickNumber and toggle isClicked
+	//If the button is clicked, increment the clickNumber and toggle commentsClicked
 	$("button[name='commentsToggleButton'").click(function(){
 		console.log("Hello");
 
-		chrome.storage.sync.get(['clickNumber', 'isClicked'], function(internal){
+		chrome.storage.sync.get(['clickNumber', 'commentsClicked'], function(internal){
 			var newClickNumber = 1;
-			var newIsClicked = true;
+			var newCommentsClicked = true;
 			//Set the new values to the old values /if they exist/
 			if(internal.clickNumber){
 				newClickNumber += internal.clickNumber; 
 			}
-			if(internal.isClicked){
-				newIsClicked = !internal.isClicked;
+			if(internal.commentsClicked){
+				newCommentsClicked = !internal.commentsClicked;
 			}
 
-			//Store the new values for clickNumber and isClicked
-			chrome.storage.sync.set({'clickNumber':newClickNumber, 'isClicked':newIsClicked});
+			//Store the new values for clickNumber and commentsClicked
+			chrome.storage.sync.set({'clickNumber':newClickNumber, 'commentsClicked':newCommentsClicked});
 			//DIsplay the new values on the popup
 			$('#clickCounter').text("Number of clicks: " + newClickNumber);
-			$('#truthHeader').text(newIsClicked.toString());
+			$('#truthHeader').text(newCommentsClicked.toString());
+			//Send a message to content script
+			chrome.tabs.query({active:true, currentWindow:true},function(tabs){
+				chrome.tabs.sendMessage(tabs[0].id, {
+					isCommented: newCommentsClicked
+				});
+			});
 
 		});
 	});
 
 	$("button[name='resetButton']").click(function(){
-		chrome.storage.sync.set({'clickNumber':0, 'isClicked':false});
+		chrome.storage.sync.set({'clickNumber':0, 'commentsClicked':false});
 		$('#clickCounter').text("Number of clicks: " + 0);
 		$('#truthHeader').text(false.toString());
 	});
