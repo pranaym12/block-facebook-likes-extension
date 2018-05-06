6a05f4b2fe78079eef5ec32999a7dfb34416c179
Pranay Mittal <pranaym12@gmail.com>
2018-05-06 13:25:21 -0700

    Add JS functionality for all the Post buttons to change the post-status depending on the button, and to send a message to Content script.

--- popup.js
+++ popup.js
@@ -46,6 +46,30 @@ $(function(){
 		});
 		$('#postStatus').text("unhidden");
 	});
+	$("button[name='post-hide-likes'").click(function(){
+		var newPost = 1;
+		chrome.storage.sync.set({'comment': newPost});
+		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
+			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
+			chrome.tabs.sendMessage(tabs[0].id, {
+				postMsg: 1
+			});				
+			//so message is sent to 0th tab
+		});
+		$('#postStatus').text("only likes are hidden");
+	});
+	$("button[name='post-hide-all'").click(function(){
+		var newPost = 2;
+		chrome.storage.sync.set({'comment': newPost});
+		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
+			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
+			chrome.tabs.sendMessage(tabs[0].id, {
+				postMsg: 2
+			});				
+			//so message is sent to 0th tab
+		});
+		$('#postStatus').text("all is hidden");
+	});
 });
 /*
 	//If the posts button is clicked, toggle the removal of likes on comments on or off 
