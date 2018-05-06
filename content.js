976aa733525a7fafd29f4a3a2557fe2dddc7962e
Pranay Mittal <pranaym12@gmail.com>
2018-05-06 11:16:59 -0700

    Clean up comments.

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-var isCommentsContent = false;
-var isPostsContent = false;
 //use event listener to see if comments enabled
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.isCommented) {	
@@ -22,19 +20,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 });
 
 chrome.storage.sync.get(['commentsClicked', 'postsClicked'], function(internal){
-
-	console.log("line 244, internalComments: " + internal.commentsClicked);
-	console.log("line 24, internalPosts: " + internal.postsClicked);
-	//For some weird reason, if statement doesn't work if I use "isCommentsContent"
 	if(internal.commentsClicked){  
 		$('.UFICommentLikeButton').css('display', "none");
 	}
-	//For some weird reason, if statement doesn't work if I use "isCommentsContent"
 	if(internal.postsClicked){  
 		$('._1g5v').css('display', "none");
 		$('._4arz').css('display', "none");
 	}
 });
-
-
-//document.getElementById("rightCol").style.display = 'none';
