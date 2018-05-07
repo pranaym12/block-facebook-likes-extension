fd753139a659d4802900896ef1115a4d311290e8
Pranay Mittal <pranaym12@gmail.com>
2018-05-06 17:56:14 -0700

    Clean up comments

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 $(function(){
 	//Check for existing stored values, and set text in popup to that
 	chrome.storage.sync.get(['post', 'comment'], function(internal){
-		//If posts-/commentsTruthDiv already exist, change the text to the stored value
-		//Because it's a boolean, in order to check if it exists, must see if its type is 'undefined'
+		//For posts and comments, set text to the proper word based on chrome storage
+		//	using a 0-2 system. 0=hidden, 1=only likes hidden, 2=everything hidden
+		//" !=='undefined' " is how I ensure that it exists, even if it's a boolean!
 		if(typeof internal.post !== 'undefined'){ 
 			if(internal.post == 0){
 				$('#postStatus').text("unhidden");
@@ -33,7 +34,9 @@ $(function(){
 		}
 	});
 
-	//get the old stored val, and if it exists, new val is opposite bc button has been clicked once
+	//For each of the 6 buttons (3 each P and C) do the same thing:
+	//When clicked, set the corresponding val in storage to the new val
+	//and send a message to the content-script
 	$("button[name='post-unhide'").click(function(){
 		var newPost = 0;
 		chrome.storage.sync.set({'post': newPost});
@@ -50,11 +53,9 @@ $(function(){
 		var newPost = 1;
 		chrome.storage.sync.set({'post': newPost});
 		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
-			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
 			chrome.tabs.sendMessage(tabs[0].id, {
 				postMsg: 1
 			});				
-			//so message is sent to 0th tab
 		});
 		$('#postStatus').text("likes hidden");
 	});
@@ -62,11 +63,9 @@ $(function(){
 		var newPost = 2;
 		chrome.storage.sync.set({'post': newPost});
 		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
-			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
 			chrome.tabs.sendMessage(tabs[0].id, {
 				postMsg: 2
 			});				
-			//so message is sent to 0th tab
 		});
 		$('#postStatus').text("all is hidden");
 	});
@@ -77,11 +76,9 @@ $(function(){
 		var newComment = 0;
 		chrome.storage.sync.set({'comment': newComment});
 		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
-			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
 			chrome.tabs.sendMessage(tabs[0].id, {
 				commentMsg: 0
 			});				
-			//so message is sent to 0th tab
 		});
 		$('#commentStatus').text("unhidden");
 	});
@@ -89,11 +86,9 @@ $(function(){
 		var newComment = 1;
 		chrome.storage.sync.set({'comment': newComment});
 		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
-			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
 			chrome.tabs.sendMessage(tabs[0].id, {
 				commentMsg: 1
 			});				
-			//so message is sent to 0th tab
 		});
 		$('#commentStatus').text("likes hidden");
 	});
@@ -101,11 +96,9 @@ $(function(){
 		var newComment = 2;
 		chrome.storage.sync.set({'comment': newComment});
 		chrome.tabs.query({active:true, currentWindow:true},function(tabs){
-			//tabs is an array of all tabs that are ACTIVE and in the CURRENTWINDOW
 			chrome.tabs.sendMessage(tabs[0].id, {
 				commentMsg: 2
 			});				
-			//so message is sent to 0th tab
 		});
 		$('#commentStatus').text("all is hidden");
 	});
