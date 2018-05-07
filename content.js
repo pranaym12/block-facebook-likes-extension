32421dad379ef797b776ce2870367103fa064c3e
Pranay Mittal <pranaym12@gmail.com>
2018-05-07 12:16:44 -0700

    Successfully block comment emoji/likes.

--- content.js
+++ content.js
@@ -1,29 +1,30 @@
 //use event listener to see if comments enabled
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.postMsg == 0) {
-		//1st is class that controls the names of friends who've liked a post
-		//2nd is class that controls # of ppl besides your friends who've liked post
-		$('._1g5v').css('display', "block"); 
-		$('._4arz').css('display', "block");	
-		
+		$('._1g5v').css('display', "block");//names of specific-friends who've liked a post
+		$('._4arz').css('display', "block");//#of non-specificfriends who've liked a post
 	}
 	if(request.postMsg == 1) {
 		$('._1g5v').css('display', "none");
 		$('._4arz').css('display', "none");	
+	}
+	if(request.postMsg ==2){
+
 	}
 	//TODO: == 2 for post and comments, and will need to update
 	//	in both 0 and 1 to unhide the things 2 hides
 
 	if(request.commentMsg == 0){
 		//this is the class that controls the # of likes shown on comments
-		$('.UFICommentLikeButton').css('display', "inline");
-		$('.UFICommentReactionsBling').css('display', "inline");
+		$('.UFICommentLikeButton').css('display', "inline");//#of likes on comment
+		$('.UFICommentReactionsBling').css('display', "inline");//emojis on comment
 	}
-	if(request.commentMsg == 1){
+	if(request.commentMsg == 1){ //show emoji, not #likes
 		$('.UFICommentLikeButton').css('display', "none");
 		$('.UFICommentReactionsBling').css('display', "inline");
 	}
-	if(request.postMsg == 2){
+	if(request.commentMsg == 2){//show emoji class, but remove class that contains emojis 
+		//	AND #likes
 		$('.UFICommentLikeButton').css('display', "inline");
 		$('.UFICommentReactionsBling').css('display', "none");
 
