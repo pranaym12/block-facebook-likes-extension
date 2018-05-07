7e6431b39d83580cd948c92bd6186c38139518b9
Pranay Mittal <pranaym12@gmail.com>
2018-05-06 18:19:57 -0700

    Corrected bug! Now I can still hide the number of likes FUNCTIONALLY! Cannot YET hide emojis. Expect next commit to do that.

--- content.js
+++ content.js
@@ -1,24 +1,25 @@
 //use event listener to see if comments enabled
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if(request.postMsg == 0) {	
-		//this is the class that controls the # of likes shown on comments
-		$('.UFICommentLikeButton').css('display', "inline");
+	if(request.postMsg == 0) {
+		//1st is class that controls the names of friends who've liked a post
+		//2nd is class that controls # of ppl besides your friends who've liked post
+		$('._1g5v').css('display', "block"); 
+		$('._4arz').css('display', "block");	
+		
 	}
 	if(request.postMsg == 1) {
-		$('.UFICommentLikeButton').css('display', "none");
+		$('._1g5v').css('display', "none");
+		$('._4arz').css('display', "none");	
 	}
 	//TODO: == 2 for post and comments, and will need to update
 	//	in both 0 and 1 to unhide the things 2 hides
 
 	if(request.commentMsg == 0){
-		//1st is class that controls the names of friends who've liked a post
-		//2nd is class that controls # of ppl besides your friends who've liked post
-		$('._1g5v').css('display', "block"); 
-		$('._4arz').css('display', "block");
+		//this is the class that controls the # of likes shown on comments
+		$('.UFICommentLikeButton').css('display', "inline");
 	}
 	if(request.commentMsg == 1){
-		$('._1g5v').css('display', "none");
-		$('._4arz').css('display', "none");	
+		$('.UFICommentLikeButton').css('display', "none");
 	}
 });
 
