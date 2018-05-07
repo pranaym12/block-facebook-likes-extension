5dd23e4ed90f6403f3f5db81c73db436b1ccc51e
Pranay Mittal <pranaym12@gmail.com>
2018-05-07 12:51:55 -0700

    Seemingly functional: posts are blocked and stored now, fully! 'Block all posts' functionality implemented. Not sure if I'll change block all post likes tho, since it removes # of comments and # of shares too

--- content.js
+++ content.js
@@ -3,13 +3,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.postMsg == 0) {
 		$('._1g5v').css('display', "block");//names of specific-friends who've liked a post
 		$('._4arz').css('display', "block");//#of non-specificfriends who've liked a post
+		$('.UFIRow.UFILikeSentence').css('display', "block");
+		$('._3399._1f6t._4_dr._20h5').css('display',"block");
 	}
-	if(request.postMsg == 1) {
+	if(request.postMsg == 1) {//Show emojis, not #likes
 		$('._1g5v').css('display', "none");
 		$('._4arz').css('display', "none");	
+		$('.UFIRow.UFILikeSentence').css('display', "block");
+		$('._3399._1f6t._4_dr._20h5').css('display',"block");
 	}
 	if(request.postMsg ==2){
-
+		$('._1g5v').css('display', "block");
+		$('._4arz').css('display', "block");
+		$('.UFIRow.UFILikeSentence').css('display', "none");
+		$('._3399._1f6t._4_dr._20h5').css('display',"none");
 	}
 	//TODO: == 2 for post and comments, and will need to update
 	//	in both 0 and 1 to unhide the things 2 hides
@@ -31,12 +38,24 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 });
 
 chrome.storage.sync.get(['post', 'comment'], function(internal){
+	//Get values from chrome storage
+	//Only runs on startup of page
+	//If, during last session, user blocked something, it's stored in storage
+	//and retrieved and automatically blocked upon next startup
+
 	console.log("post: " + internal.post+", comment: "+internal.comment);
 	if(internal.post == 1){  
 		$('._1g5v').css('display', "none");
-		$('._4arz').css('display', "none");
+		$('._4arz').css('display', "none");	
+		$('.UFIRow.UFILikeSentence').css('display', "block");
+		$('._3399._1f6t._4_dr._20h5').css('display',"block");
 	}
-	//TODO == 2 for post and comments
+	if(internal.post ==2){
+		$('._1g5v').css('display', "block");
+		$('._4arz').css('display', "block");
+		$('.UFIRow.UFILikeSentence').css('display', "none");
+		$('._3399._1f6t._4_dr._20h5').css('display',"none");
+	}
 	if(internal.comment == 1){ //show emoji, not #likes
 		console.log("comment: "+internal.comment+", console.log line41");
 		$('.UFICommentLikeButton').css('display', "none");
