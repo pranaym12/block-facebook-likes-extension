7f09ff2ae7b043cc8636ddd33cb4ed206e7a71ca
Pranay Mittal <pranaym12@gmail.com>
2018-05-07 21:35:35 -0700

    Clean up redundant code by grouping into a function for each type of hide or unhide. post0, post1, post2, comment1, comment2, comment3. Very clear!

--- content.js
+++ content.js
@@ -1,39 +1,24 @@
-//use event listener to see if comments enabled
+//Any time message comes from popup (when user presses a button)
+//	update page based on the message. 
+//0 = nothing hidden; 1 = like # hidden; 2 = like # and emoji hidden
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.postMsg == 0) {
-		$('._1g5v').css('display', "block");//names of specific-friends who've liked a post
-		$('._4arz').css('display', "block");//#of non-specificfriends who've liked a post
-		$('.UFIRow.UFILikeSentence').css('display', "block");
-		$('._3399._1f6t._4_dr._20h5').css('display',"block");
+		post0();
 	}
 	if(request.postMsg == 1) {//Show emojis, not #likes
-		$('._1g5v').css('display', "none");
-		$('._4arz').css('display', "none");	
-		$('.UFIRow.UFILikeSentence').css('display', "block");
-		$('._3399._1f6t._4_dr._20h5').css('display',"block");
+		post1();
 	}
 	if(request.postMsg ==2){
-		$('._1g5v').css('display', "block");
-		$('._4arz').css('display', "block");
-		$('.UFIRow.UFILikeSentence').css('display', "none");
-		$('._3399._1f6t._4_dr._20h5').css('display',"none");
+		post2();
 	}
-	//TODO: == 2 for post and comments, and will need to update
-	//	in both 0 and 1 to unhide the things 2 hides
-
 	if(request.commentMsg == 0){
-		//this is the class that controls the # of likes shown on comments
-		$('.UFICommentLikeButton').css('display', "inline");//#of likes on comment
-		$('.UFICommentReactionsBling').css('display', "inline");//emojis on comment
+		comment0();
 	}
-	if(request.commentMsg == 1){ //show emoji, not #likes
-		$('.UFICommentLikeButton').css('display', "none");
-		$('.UFICommentReactionsBling').css('display', "inline");
+	if(request.commentMsg == 1){
+		comment1();
 	}
-	if(request.commentMsg == 2){//show emoji class, but remove class that contains emojis 
-		//	AND #likes
-		$('.UFICommentLikeButton').css('display', "inline");
-		$('.UFICommentReactionsBling').css('display', "none");
+	if(request.commentMsg == 2){
+		comment2();
 	}
 });
 
@@ -47,35 +32,60 @@ function refreshLikes(){
 		//Only runs on startup of page
 		//If, during last session, user blocked something, it's stored in storage
 		//and retrieved and automatically blocked upon next startup
-
-		console.log("post: " + internal.post+", comment: "+internal.comment);
+		//console.log("post: " + internal.post+", comment: "+internal.comment);
 		if(internal.post == 1){  
-			$('._1g5v').css('display', "none");
-			$('._4arz').css('display', "none");	
-			$('.UFIRow.UFILikeSentence').css('display', "block");
-			$('._3399._1f6t._4_dr._20h5').css('display',"block");
+			post1();
 		}
 		if(internal.post ==2){
-			$('._1g5v').css('display', "block");
-			$('._4arz').css('display', "block");
-			$('.UFIRow.UFILikeSentence').css('display', "none");
-			$('._3399._1f6t._4_dr._20h5').css('display',"none");
+			post2();
 		}
 		if(internal.comment == 1){ //show emoji, not #likes
 			//console.log("comment: "+internal.comment+", console.log line41");
-			$('.UFICommentLikeButton').css('display', "none");
-			$('.UFICommentReactionsBling').css('display', "inline");
+			comment1();
 		}
 		if(internal.comment == 2){//show emoji class, but remove class that contains emojis 
 			//	AND #likes
 			//console.log("comment: "+internal.comment+", console.log line47");
-			$('.UFICommentLikeButton').css('display', "inline");
-			$('.UFICommentReactionsBling').css('display', "none");
+			comment2();
 		}
 	});
 	//Every 1s, refreshLikes called. recursive timer
 	setTimeout(refreshLikes, 1000); 
 }
+
+function post0(){//post likes unhidden
+	$('._1g5v').css('display', "block");//names of specific-friends who've liked a post
+	$('._4arz').css('display', "block");//#of non-specificfriends who've liked a post
+	$('.UFIRow.UFILikeSentence').css('display', "block");
+	$('._3399._1f6t._4_dr._20h5').css('display',"block");	
+}
+function post1(){//post like #s hidden
+	$('._1g5v').css('display', "none");
+	$('._4arz').css('display', "none");	
+	$('.UFIRow.UFILikeSentence').css('display', "block");
+	$('._3399._1f6t._4_dr._20h5').css('display',"block");
+}
+function post2(){//post like# and like-emoji hidden
+	//	and if sponsored post, # of shares and comments hidden
+	//	entire "like-status bar" basically
+	$('._1g5v').css('display', "block");
+	$('._4arz').css('display', "block");
+	$('.UFIRow.UFILikeSentence').css('display', "none");
+	$('._3399._1f6t._4_dr._20h5').css('display',"none");
+}
+function comment0(){//If comment likes are unhidden
+	$('.UFICommentLikeButton').css('display', "inline");//#of likes on comment
+	$('.UFICommentReactionsBling').css('display', "inline");//emojis on comment
+}
+function comment1(){//If comment like #s hidden
+	$('.UFICommentLikeButton').css('display', "none");
+	$('.UFICommentReactionsBling').css('display', "inline");
+}
+function comment2(){//comment like# and like-emoji hidden
+	$('.UFICommentLikeButton').css('display', "inline");
+	$('.UFICommentReactionsBling').css('display', "none");
+}
+
 /*
 var num = 0;
 counter();
