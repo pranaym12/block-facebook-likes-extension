a0b5205ab7aa7aa29fe5230d4f1e188f482e3f66
Pranay Mittal <pranaym12@gmail.com>
2018-05-09 18:13:52 -0700

    Remove redundant, long calls to segmented-control-stuff. Replace with one-line calls to a function that does it for each button: post and comment: 0, 1, and 2.

--- popup.js
+++ popup.js
@@ -1,48 +1,47 @@
 function post0status(){
 	$('#postStatus').text("unhidden");
-	$("#post0").checked = true;
-
+	/*$("#post0").checked = true;
 	$('#post0').parent().addClass("checked");
 	$('#post1').removeClass("checked");
-	$('#post2').removeClass("checked");	
+	$('#post2').removeClass("checked");	*/
+	updateSC("post", 0);
 }function post1status(){
 	$('#postStatus').text("likes hidden");
-	$("#post1").checked = true;
-
+	/*$("#post1").checked = true;
 	$('#post1').parent().addClass("checked");
 	$('#post0').removeClass("checked");
-	$('#post2').removeClass("checked");
+	$('#post2').removeClass("checked");*/
+	updateSC("post", 1);
 }function post2status(){
 	$('#postStatus').text("all is hidden");
-	$("#post2").checked = true;
-
+	/*$("#post2").checked = true;
 	$('#post2').parent().addClass("checked");
 	$('#post1').removeClass("checked");
-	$('#post0').removeClass("checked");
-}
-function comment0status(){
+	$('#post0').removeClass("checked");*/
+	updateSC("post", 2);
+}function comment0status(){
 	$('#commentStatus').text("unhidden");
-	$("#comment1").checked = true;
-
+	/*$("#comment1").checked = true;
 	$('#comment0').parent().addClass("checked");
 	$('#comment1').removeClass("checked");
-	$('#comment2').removeClass("checked");
+	$('#comment2').removeClass("checked");*/
+	updateSC("comment", 0);
 }function comment1status(){
 	$('#commentStatus').text("likes hidden");
-	$("#comment1").checked = true;
-
+	/*$("#comment1").checked = true;
 	$('#comment1').parent().addClass("checked");
 	$('#comment0').removeClass("checked");
-	$('#comment2').removeClass("checked");
+	$('#comment2').removeClass("checked");*/
+	updateSC("comment", 1);
 }function comment2status(){
 	$('#commentStatus').text("all is hidden");
-	$("#comment2").checked = true;
-
+	/*$("#comment2").checked = true;
 	$('#comment2').parent().addClass("checked");
 	$('#comment1').removeClass("checked");
-	$('#comment0').removeClass("checked");
+	$('#comment0').removeClass("checked");*/
+	updateSC("comment", 2);
 }
-$(function(){
+$(function(){//On startup
 	//Check for existing stored values, and set text in popup to that
 	chrome.storage.sync.get(['post', 'comment'], function(internal){
 		//For posts and comments, set text to the proper word based on chrome storage
@@ -69,7 +68,6 @@ $(function(){
 		}else{
 			comment2status();
 		}
-		
 	});
 
 	//For each of the 6 buttons (3 each P and C) do the same thing:
@@ -142,6 +140,41 @@ $(function(){
 			comment2status();
 	});
 });
+
+function updateSC(pC, num){
+	//updates Segmented Control to the right value on post or comment
+	//Function takes string pC = "post" or "comment"
+	//and num: 0, 1, 2; which creates str: "0", "1", "2"
+
+	//First check that 0<=num<=2, and assign 2 other numbers
+	if(num==0){
+		var str="0";//string version of num
+		var otr1 = "1";
+		var otr2 = "2";
+	}else if(num==1){
+		var str="1";
+		var otr1 = "0";
+		var otr2 = "2";
+	}else if(num==2){
+		var str="2";
+		var otr1 = "0";
+		var otr2 = "1";
+	}
+	//Then fill in all the things that need to be done if pC==post"||comment"	
+	if(pC == "post" || pC == "comment"){
+		pC = "#" + pC;
+		$(pC+str).checked=true; //'Check' the physical input-radio
+		//class "checked" creates segmented-control bar
+		$(pC+str).parent().addClass("checked");
+		$(pC+otr1).removeClass("checked");
+		$(pC+otr2).removeClass("checked");
+	}
+	//EXAMPLE: updateSC("post", 0) yields:
+	/*$("#post0").checked = true;
+	$('#post0').parent().addClass("checked");
+	$('#post1').removeClass("checked");
+	$('#post2').removeClass("checked");	*/
+}
 
 /*
 	//Buttons for the Comments
